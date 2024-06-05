4e163d7db073bf4503c186232fb924fb7b3db861
Anthony Ogundipe <diltony@gmail.com>
2024-06-05 03:47:32 +0100

    Updated files

--- src/Controllers/PreviewController.php
+++ src/Controllers/PreviewController.php
@@ -1,16 +1,16 @@
 <?php
+
 namespace Dhtml\Composer\Controllers;
 
-use Laminas\Diactoros\Response\HtmlResponse;
-use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Laminas\Diactoros\Response\HtmlResponse;
 
-class PreviewController implements RequestHandlerInterface
+class PreviewController
 {
-    public function handle(Request $request): Response
+    public function handle(Request $request): ResponseInterface
     {
-        return new HtmlResponse('<h1>Hello, world!</h1>');
+        $html = '<html><body><h1>Hello World</h1></body></html>';
+        return new HtmlResponse($html);
     }
 }
-
